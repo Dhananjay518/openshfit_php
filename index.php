9497b59a8a78955327e2b29e558c89ee4daeea34
Dhananjay518 <78597882+Dhananjay518@users.noreply.github.com>
2023-03-02 15:02:53 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,12 @@
 <html>
 <body>
 
-<h1>My first PHP page for webhooks 1234 </h1>
+<h1>My php application </h1>
 
 <?php
 echo "Hello World! From DK";
- echo "hello from UNNATI 555555.........";
+ :wq
+  
 ?>
 
 </body>
